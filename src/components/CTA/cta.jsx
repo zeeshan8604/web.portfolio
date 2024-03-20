abf103eafa750f59054d0d1030b6edfe53c35b91
Zeeshan Ahmed <zeeshanahmed860443@gmail.com>
2024-03-20 08:24:40 +0530

    Brief description of the changes you made

--- src/components/CTA/cta.jsx
+++ src/components/CTA/cta.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
-import CV from '../../assets/cv.pdf'
+import React from "react";
+import CV from "../../assets/cv.pdf";
 
 const cta = () => {
   return (
+    //added
     <div className="cta">
-        <a href={CV} download className='btn'>Download CV</a>
-        <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+      <a href={CV} download className="btn">
+        Download CV
+      </a>
+      <a href="#contact" className="btn btn-primary">
+        Let's Talk
+      </a>
     </div>
+  );
+};
 
-  )
-}
-
-export default cta
+export default cta;
